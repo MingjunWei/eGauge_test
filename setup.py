6ffc12d042588c5985be5a87c35639a29ca5afe5
David Mosberger-Tang <davidm@egauge.net>
2020-07-16 13:03:13 -0600

    v0.2.2 released

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setuptools.setup(
     name='egauge-python',
-    version='0.21',
+    version='0.2.2',
     packages=setuptools.find_namespace_packages(include='egauge.*'),
     install_requires=['crcmod', 'intelhex', 'wheel', 'pexpect', 'requests'],
     include_package_data=True,
