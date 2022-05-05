0014f9dd2dde38db6169890537843e667c76322e
David Mosberger-Tang <davidm@egauge.net>
2022-05-05 13:41:21 -0600

    v0.5.9 released

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setuptools.setup(
     name="egauge-python",
-    version="0.5.8",
+    version="0.5.9",
     packages=setuptools.find_namespace_packages(include="egauge.*"),
     install_requires=[
         "crcmod",
